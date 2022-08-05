340f88a7e6ebb6d427182e79a454cf780d0bb341
gladja <gladja@yandex.ru>
2022-08-05 20:48:14 +0300

    added contend and section

---
+++ project_js/js/script.js
@@ -0,0 +1,33 @@
+let number=5;
+// alert(number);
+
+// let num=10;
+// num=7;
+
+
+// const pi = 3.14;
+
+// let modal;
+// console.log(modal);
+
+// console.log( 4 + '4');
+
+
+// let x = 4;
+// x--;
+// console.log(x)
+
+// let answer = confirm('you heve 18 year?');
+// console.log(answer);
+
+//let answer = prompt('you heve 18 year?','');
+//console.log(answer);
+
+let answers = [];
+
+answers[0] = prompt('Whot is your first name?','');
+answers[1] = prompt('Whot is your last name?','');
+answers[2] = prompt('How old are you?','');
+
+// console.log(answers);
+document.write(answers);
