9740e5e35c774c564b2e13a23848fde52b464a52
gladja <gladja@yandex.ru>
2022-10-14 08:54:51 +0300

    added code in projaec_react

--- project_react/src/components/Dialogs/Dialogs.jsx
+++ project_react/src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,35 @@
 import {React} from "react";
-// import css from './Dialogs.module.css';
+import css from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const Dialog = (props) => {
+    let path = '/dialogs/' + props.id;
+    return (
+        <div className={css.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+}
+
+const Message = (props) => {
+    return <div className={css.message}>{props.post}</div>
+}
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={css.dialogs}>
+            <div className={css.dialogsItem}>
+                <Dialog name='Alex' id='1'/>
+                <Dialog name='Stas' id='2'/>
+                <Dialog name='Luda' id='3'/>
+                <Dialog name='Ivan' id='4'/>
+            </div>
+            <div className={css.messages}>
+                <Message post='Hi'/>
+                <Message post='Hello'/>
+                <Message post='Yo'/>
+                <Message post='How are you?'/>
+            </div>
         </div>
     );
 };
--- project_react/src/components/Navbar/Navbar.jsx
+++ project_react/src/components/Navbar/Navbar.jsx
@@ -7,18 +7,18 @@ const Navbar = () => {
     return (
         <nav className={css.nav}>
             <div className={css.item}>
-                <NavLink to='/profile'>Profile</NavLink>
+                <NavLink to='/profile' className={({ isActive }) => (isActive ? `${css.active}` : 'inactive')}>Profile</NavLink>
             </div>
-            <div>
-                <NavLink to='/dialogs'>Messages</NavLink>
+            <div className={css.item}>
+                <NavLink to='/dialogs' className={({ isActive }) => (isActive ? `${css.active}` : '')}>Messages</NavLink>
             </div>
-            <div>
+            <div className={css.item}>
                 <a href='/news'>News</a>
             </div>
-            <div>
+            <div className={css.item}>
                 <a href='/music'>Music</a>
             </div>
-            <div>
+            <div className={css.item}>
                 <a href='/settings'>Settings</a>
             </div>
         </nav>
