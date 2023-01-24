b64b6ab462614d70214d3f9ac6eb7ae8e0e06665
gladja <gladja@yandex.ru>
2023-01-24 22:38:14 +0200

    js codewar learn

--- project_js/js/test.js
+++ project_js/js/test.js
@@ -1,7 +1,12 @@
-function reverseWords(str) {
-  const stringRevers = str.split("").reverse().join("");
-  const wordRevers = stringRevers.split(" ").reverse().join(" ");
-  console.log(wordRevers);
-  return wordRevers;
-}
-reverseWords("The quick brown fox jumps over the lazy dog.");
+var number = function(busStops){
+
+  let sum = 0
+  const sunBusStop = busStops.map(item => sum += (item[0]-item[1]));
+  // sunBusStop.map((item ,idx) =>  sum += item);
+  console.log(sunBusStop);
+  console.log(sum);
+  return sum;
+    };
+
+number([[10,0],[3,5],[5,8]]); //5
+number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]); //17
