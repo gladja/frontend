74e92b79172129371a0b12fa88b51ec105a02c0c
gladja <gladja@yandex.ru>
2023-02-05 17:47:43 +0200

    decided js task on codewar

--- project_js/js/learn/Object/_tasks.js
+++ project_js/js/learn/Object/_tasks.js
@@ -143,3 +143,54 @@ function nameСheck(nameFirst) {
 
     console.log(JSON.stringify(financeReport1));
 //-----------------------------------------------ok
+
+
+
+    // return ...
+//     console.log(calc());
+//result of calc method should be equal to
+// {
+// viktor: {
+//         apple: 2,
+//         grape: 1,
+//         orange: 1,
+//         banana: 1,
+//         },
+//   kate: {
+//         apple: 1,
+//         grape: 2,
+//         orange: 1,
+//         banana: 1,
+//         }.
+//}
+// 3anpeщeнo обpaщаться Ha npямую  obj.viktor и obj.kate
+
+    const obj = {
+        viktor: ["apple", "grape", "orange", "apple", "banana"],
+        kate: ["grape", "orange", "apple", "grape", "banana"],
+    };
+
+    function calc() {
+        const { viktor } = obj;
+        // console.log(viktor);
+        const vi = {};
+        for (let v of viktor) {
+            vi[v] = (vi[v] || 0) + 1;
+        }
+        // console.log(vi);
+
+        const { kate } = obj;
+        // console.log(kate);
+        const ka = {};
+        for (let k of kate) {
+            ka[k] = (ka[k] || 0) + 1;
+        }
+        // console.log(ka);
+
+        obj.viktor = vi;
+        obj.kate = ka;
+
+        return obj;
+    }
+    console.log(calc());
+//-----------------------------------------------ok
