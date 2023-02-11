d4686417dacb23cbe51b5fc660a32ef62de73f9b
gladja <gladja@yandex.ru>
2023-02-11 15:32:21 +0200

    refactor code

--- project_js/js/test2.js
+++ project_js/js/test2.js
@@ -1,22 +1,21 @@
-const arrayOne = [3, 2, 5, 6, 10, 11, 6, 8, 9, 13];
-const arrayTwo = [4, 8, 9, 12, 3, 4, 5, 15, 10, 5];
+const widths = [3, 2, 5, 6, 10, 11, 6, 8, 9, 13];
+const heights = [4, 8, 9, 12, 3, 4, 5, 15, 10, 5];
 
-let width = 0;
-let height = 0;
+let widthHigher = 0;
+let heightHigher = 0;
 let bigDiagonal = 0;
 
-for (let i = 0; i < arrayOne.length; i++) {
-  for (let j = 0; j < arrayTwo.length; j++) {}
-  if (arrayOne[i] > arrayTwo[i]) {
-    width++;
+for (let i = 0; i < widths.length; i++) {
+  if (widths[i] > heights[i]) {
+    widthHigher++;
   } else {
-    height++;
+    heightHigher++;
   }
-  const c = Math.pow(arrayOne[i], 2) + Math.pow(arrayTwo[i], 2);
+  const c = Math.pow(widths[i], 2) + Math.pow(heights[i], 2);
   const diagonal = Math.sqrt(c);
   if (diagonal > bigDiagonal) {
-    bigDiagonal = diagonal;
+    bigDiagonal = i;
   }
 }
 console.log(bigDiagonal);
-console.log(`width: ${width} height: ${height}`);
+console.log(`width: ${widthHigher} height: ${heightHigher}`);
