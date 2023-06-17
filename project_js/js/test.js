70d51d0d42ea4be803e1309361b98413ed742b11
gladja <gladja@Mac-mini-gladja.local>
2023-06-17 15:08:36 +0300

    js task

--- project_js/js/test.js
+++ project_js/js/test.js
@@ -1,14 +1,47 @@
-function expressionMatter(a, b, c) {
-    let num1 = a * (b + c);
-    let num2 = a * b * c;
-    let num3 =a + b * c;
-    let num4 =(a + b) * c;
+const atTheOldToad = {
+    potions: [
+        {name: "Speed potion", price: 460},
+        {name: "Dragon breath", price: 780},
+        {name: "Stone skin", price: 520},
+    ],
+    // Change code below this line
 
-    let array = [num1, num2, num3, num4];
-    let max = Math.max(...array);
-    console.log(max);
-    return max;
-}
-expressionMatter(2, 1, 2) // 6
-expressionMatter(2, 1, 1) // 4
+    getPotions() {
+        return this.potions;
+    },
+    addPotion(newPotion) {
+        const {name, price} = newPotion
 
+        for (const potion of this.potions) {
+            if (potion.name === name) {
+                return `Error! Potion ${name} is already in your inventory!`;
+            }
+        }
+        return this.potions.push(newPotion);
+    },
+    removePotion(potionName) {
+
+        for (let i = 0; i < this.potions.length; i += 1) {
+
+            if (this.potions[i].name === potionName) {
+                this.potions.splice(i, 1);
+            }
+        }
+        return `Potion ${potionName} is not in inventory!`;
+    },
+    updatePotionName(oldName, newName) {
+
+        for (const potion of this.potions) {
+            console.log(potion.name)
+            if (potion.name === oldName) {
+                potion.name = newName
+            }
+        }
+
+        return `Potion ${oldName} is not in inventory!`;
+    },
+    // Change code above this line
+};
+
+console.log(atTheOldToad.updatePotionName("Dragon breath", "Polymorth"))
+console.log(atTheOldToad.potions)
--- project_js/js/test2.js
+++ project_js/js/test2.js
@@ -1,22 +1,47 @@
-const uniqueInOrder=function(iterable){
-    console.log(iterable.split(''))
-    // let newArray = [...new Set(iterable)]
-    // console.log(newArray)
-    let arr = []
-    let newArray = [];
-    iterable.split('').forEach((item, i, arr) => {
-        console.log(i)
-        console.log(iterable[i])
-        console.log(item)
-        console.log(arr)
-        newArray = arr.indexOf(item)
-        console.log(newArray)
-        arr.push(iterable[newArray])
 
-        if (newArray === arr) {
-            newArray
+
+
+//  Напишіть функцію updateObject, яка приймає об'єкт і повертає новий об'єкт без вказаних параметрів
+// Результат, що очікується ({a: 1, b: 2, c: 3}, 'b', 'a') => {c: 3}
+
+function updateObject (obj, ...rest){
+    console.log(obj)
+    console.log(rest)
+
+    const newObj = {};
+
+    const newArr = Object.keys(obj)
+    console.log(newArr)
+    for (const key of newArr) {
+        if(!rest.includes(key)) {
+            console.log(key)
+
+           newObj[key] = obj[key]
+            console.log(newObj)
         }
-            console.log(arr)
-    })
+    }
+    return newObj
 }
-uniqueInOrder('AAAABBBCCDAABBB')  //['A', 'B', 'C', 'D', 'A', 'B']
+
+console.log(updateObject({a: 1, b: 2, c: 3}, 'b', 'a'))
+
+
+// https://leetcode.com/
+
+
+// const arrNumber = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// Напишіть функцію, яка розвертає масив у зворотньому напрямку.Метод reverse() не використовувати
+
+// Напишіть функцію, яка очистить масив від небажаних значень, таких як false, undefined, порожні рядки, нуль, null
+// Результат, що очікується[0, 1, false, 2, undefined, '', 3, null] => [1, 2, 3]
+
+// Напишіть функцію, яка повертає новий масив без вказаних значень.Використовуйте примітиви
+// Очікується [1, 2, 3, 1, 2] без 1, 2 => [1,2,3]
+
+
+
+// Напиши клас Notes, який керує колекцієї заміток у властивості items
+// Замітка - це об"єкт із властивостями text та priority
+// Додай класу статичну властивість Priopity,
+//     в якому буде зберігатися об"єкт з пріоритетами
+//     Додай методи addNote(note), removeNote(text), updatePriority(text, newPriority)
